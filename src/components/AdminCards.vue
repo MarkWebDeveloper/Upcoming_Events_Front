b53d37ad12ffd341ad8d735ff13fd12ab18fa704
Luis Pardo <luispardosuarez@gmail.com>
2024-02-29 02:42:42 +0100

    Added data loading to the card and  delete button to teh AdminCards.Vue

--- src/components/AdminCards.vue
+++ src/components/AdminCards.vue
@@ -1,97 +1,97 @@
 <script setup lang="ts">
+import axios from 'axios';
+import type Event from '@/models/Event';
 
+const props = defineProps<{
+  events: Event[]
+}>();
+
+const deleteEvent = async (eventId: number) => {
+  try {
+    await axios.delete(`http://localhost:8080/api/v1/events/${eventId}`);
+  } catch (error) {
+    console.error('Error al eliminar el evento:', error);
+  }
+};
 </script>
 
 <template>
-
-    <div id="cards">
-
-        <img src="/src/assets/img/Jornadas-de-la-Matanza-2024-FIT-Amieva.jpg" alt="" class="mainImage">
-
-        <h2>JORNADAS DE LA MATANZA</h2>
-
-        <div class="location">
-
-            <img src="/src/assets/img/place-svgrepo-com.svg" alt="">
-            <p>AMIEVA</p>
-
-        </div>
-
-        <div class="date">
-
-            <img src="/src/assets/img/calendar-days-svgrepo-com.svg" alt="">
-            <p>02/02/2024 ~ 04/02/24</p>
-
-        </div>
-
-        <div class="buttonContainer">
-
-            <img src="/src/assets/img/star-svgrepo-com.svg" alt="">
-            <button>EDITAR</button>
-            <img src="/src/assets/img/trash-svgrepo-com.svg" alt="">
-
-        </div>
-
+  <div id="cards-container">
+    <div v-for="event in events" :key="event.id" class="card">
+      <img :src="`/src/assets/img/${event.event_image}`" alt="" class="mainImage">
+      <h2>{{ event.event_title }}</h2>
+      <div class="location">
+        <img src="/src/assets/img/place-svgrepo-com.svg" alt="">
+        <p>{{ event.city.nameOfCity }}</p>
+      </div>
+      <div class="date">
+        <img src="/src/assets/img/calendar-days-svgrepo-com.svg" alt="">
+        <p>{{ event.start_date }} ~ {{ event.finish_date }}</p>
+      </div>
+      <div class="buttonContainer">
+        <img src="/src/assets/img/star-svgrepo-com.svg" alt="">
+        <button @click="deleteEvent(event.id)">ELIMINAR</button>
+        <img src="/src/assets/img/trash-svgrepo-com.svg" alt="">
+      </div>
     </div>
-        
+  </div>
 </template>
 
 <style scoped lang="scss">
-
 #cards {
 
+width: 60%;
+border: 2px solid #27a138; 
+border-radius: 5%;
+padding: 10px; 
+
+text-align: center;
+font-family: 'Raleway', sans-serif;
+
+.location, .date {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.mainImage {
     width: 60%;
-    border: 2px solid #27a138; 
-    border-radius: 5%;
-    padding: 10px; 
+    padding: 10px;
+}
 
-    text-align: center;
-    font-family: 'Raleway', sans-serif;
+img {
+    width: 10%;
+}
 
-    .location, .date {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-    }
+h2 {
+    font-weight: bold;
+    padding: 5px;
+}
 
-    .mainImage {
-        width: 60%;
-        padding: 10px;
-    }
+p {
+    color: #27a138;
+    font-weight: bold;
+    padding: 5px;
+}
+
+.buttonContainer {
+
+    display: flex;
+    justify-content: space-between;
+    padding: 5px;
 
     img {
-        width: 10%;
+        width: 12%;
     }
 
-    h2 {
-        font-weight: bold;
-        padding: 5px;
-    }
-
-    p {
-        color: #27a138;
-        font-weight: bold;
-        padding: 5px;
-    }
-
-    .buttonContainer {
-
-        display: flex;
-        justify-content: space-between;
-        padding: 5px;
-
-        img {
-            width: 12%;
-        }
-    
-    button {
-        background-color: #27a138;
-        color: white;
-        font-weight: bold;
-        border-radius: 15px;
-        padding: 10px;
-        margin-top: 10px;
-    }
+button {
+    background-color: #27a138;
+    color: white;
+    font-weight: bold;
+    border-radius: 15px;
+    padding: 10px;
+    margin-top: 10px;
+}
 
 }
 
@@ -101,19 +101,19 @@
 @media only screen and (min-width: 768px) {
 
 #cards {
-  width: 15%;
+width: 15%;
 
-  h2 {
-    font-size: 1rem;
-  }
+h2 {
+font-size: 1rem;
+}
 
-  img {
-    width: 10%;
-  }
+img {
+width: 10%;
+}
 
-  .mainImage {
-    width: 40%;
-  }
+.mainImage {
+width: 40%;
+}
 }
 }
 </style>
