1e89953d813572a70db3014be4f5798c63f65afd
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-02-29 09:48:43 +0100

    Update RegistroView.vue

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import RegistroUsuario from '@/components/RegistroUsuario.vue';
 
-
-
-
-=======
-import RegistroUsuario from "@/components/RegistroUsuario.vue";
->>>>>>> 41b273f3f68ac38f3ee0f200ea450800823bdab8
 </script>
 <template>
   <div>
